67f7f2c272e98afc29dcd8ab8efec2f96c8877c8
Sajjan Nehal <sajjannehal@gmail.com>
2020-04-18 14:20:49 +0530

    code complete but bugs left

--- main.go
+++ main.go
@@ -7,26 +7,95 @@ import(
 )
 
 var opstack Stack
+var postfix string
 
-func IsOperator(C uint8) bool{
-	return strings.ContainsAny(string(C), "+ & - & * & /")
+func IsOperator(C string) bool{
+	return strings.ContainsAny(C, "+ & - & * & /")
+}
+
+func PopStackUntilLeftParenthesis(){
+	for opstack.Top().(string) != "(" {
+		postfix += opstack.Top().(string)
+		opstack.Pop()
+	}
+}
+
+func EqualPrecedence(nextchar string){
+	postfix += opstack.Top().(string)
+	opstack.Pop()
+}
+
+func GetOperatorWeight(op string) int{
+	weight := -1
+	switch op {
+		case "+","-":
+			weight = 1
+			break
+		case "*", "/":
+			weight = 2
+			break
+		case "^":
+			weight = 3
+			break
+	}
+	return weight
+}
+
+func IsPresHigh(nextchar string, stacktop string) bool{
+	nextchar_weight := GetOperatorWeight(nextchar)
+	stacktop_weight := GetOperatorWeight(stacktop)
+
+	if nextchar_weight == stacktop_weight{
+		EqualPrecedence(nextchar)
+		return true
+	}else if nextchar_weight > stacktop_weight{
+		return true
+	}else{
+		return false
+	}
+}
+
+func PopStackUntilHigherPres(nexttop string){
+	for !IsPresHigh(nexttop, opstack.Top().(string)){
+		postfix += opstack.Top().(string)
+		opstack.Pop()
+		fmt.Println(postfix)
+	}
+}
+
+func PopStackUntilEmpty(){
+	for opstack.Empty(){
+		postfix += opstack.Top().(string)
+		opstack.Pop()
+	}
 }
 
 func IteratesString(input_infix string){
 	for i := 0; i < len(input_infix); i++ {
-		if IsOperator(input_infix[i]){
-			if opstack.Empty(){
-				opstack.Push(input_infix[i])
+		if IsOperator(string(input_infix[i])){
+			
+			fmt.Println(opstack.Empty())
+
+			if opstack.Empty() || opstack.Top().(string) == "(" || IsPresHigh(string(input_infix[i]), opstack.Top().(string)){
+				opstack.Push(string(input_infix[i]))
+			}else if !IsPresHigh(string(input_infix[i]), opstack.Top().(string)){
+				PopStackUntilHigherPres(string(input_infix[i]))
 			}
-			// fmt.Println(string(input_infix[i]))
+		}else if string(input_infix[i]) == "(" {
+			opstack.Push(string(input_infix[i]))
+		}else if string(input_infix[i]) == ")" {
+			PopStackUntilLeftParenthesis()
 		}else{
-			fmt.Println(string(input_infix[i]))
+			postfix += string(input_infix[i])
 		}
 	}
+	PopStackUntilEmpty()
 }
 
 func main(){
 	user_input := os.Args[1]
 
 	IteratesString(user_input)
+
+	fmt.Println(postfix)
 }
